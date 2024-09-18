0b218227b1b4af87a3e5bb82933acda611dba18a
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2024-09-18 20:50:54 +0200

    started exercise4

--- 1-EV1/introduction/src/exercise4MyDate/MyDate.java
+++ 1-EV1/introduction/src/exercise4MyDate/MyDate.java
@@ -16,4 +16,50 @@ public class MyDate
     public int getMonth() {
         return month;
     }
+
+    public int getDay()
+    {
+        return day;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    public void setMonth(int month) throws IllegalArgumentException
+    {
+        if (month <= 0 || month > 12)
+            throw new IllegalArgumentException(Utils.MONTH_NOT_VALID);
+        this.month = month;
+    }
+
+    public void setDay(int day) throws IllegalArgumentException
+    {
+        if (!isDayValid(day))
+            throw new IllegalArgumentException(Utils.DAY_NOT_VALID);
+    }
+
+    private boolean isDayValid(int day)
+    {
+        if (day > 0 || day <= 31)
+            return true;
+        if (day == 31 && monthHas31())
+            return true;
+        if (day == 30 || day == 29 && month != 2)
+            return true;
+        if (day isYearLeap())
+            return true;
+        return false;
+    }
+
+    private boolean monthHas31()
+    {
+        return month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12;
+    }
+
+    private boolean isYearLeap()
+    {
+        return true;
+    }
 }
---
+++ 1-EV1/introduction/src/exercise4MyDate/Utils.java
@@ -0,0 +1,7 @@
+package exercise4MyDate;
+
+public class Utils
+{
+    final static String MONTH_NOT_VALID = "Month is not valid.";
+    final static String DAY_NOT_VALID = "Day is not valid.";
+}
