689c9f2696bfb081b9af3d45263e8740e07d6508
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2024-10-03 18:27:29 +0200

    updated 5.4.2

--- 1-EV1/unit2/activity5.4.2/src/Main.java
+++ 1-EV1/unit2/activity5.4.2/src/Main.java
@@ -6,38 +6,43 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        main("file1.txt", "file2.txt", "output_sorted.txt");
 
     }
 
-    public static void main(String path1, String path2)
+    public static void main(String path1, String path2, String output)
     {
         File file1 = new File(path1);
         File file2 = new File(path2);
-        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file1,false)));
+        File outputFile = new File(output);
+        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outputFile,false)));
                 BufferedReader reader1 = new BufferedReader(new FileReader(file1));
                 BufferedReader reader2 = new BufferedReader(new FileReader(file2)))
         {
-            if (!file1.exists())
-                if (file1.createNewFile()) throw new IllegalArgumentException();
-            List<String> content1 = new ArrayList<>();
-            List<String> content2 = new ArrayList<>();
-            for (String line1 = reader1.readLine(); line1 != null;)
+            if (!outputFile.exists())
+                if (outputFile.createNewFile()) throw new IllegalArgumentException();
+            String line1 = new String();
+            String line2 = new String();
+            while (true)
             {
-                content1.add(line1);
-                line1 = reader1.readLine();
+                if (line1 == null)
+                    line1 = reader1.readLine();
+                if (line2 == null)
+                    line2 = reader2.readLine();
+                if (line1 == null && line2 == null)
+                        break;
+
+                if (line2 == null || line1 != null && line1.compareTo(line2) < 0)
+                {
+                    writer.println(line1);
+                    line1 = null;
+                }
+                else if (line2 != null)  // if line1.compareTo(line2) > 0 is implied
+                {
+                    writer.println(line2);
+                    line2 = null;
+                }
             }
-            for (String line2 = reader1.readLine(); line2 != null;)
-            {
-                content2.add(line2);
-                line2 = reader1.readLine();
-            }
-
-            List<String> result = new ArrayList<>();
-            while
-
-
-
         }
         catch (FileNotFoundException e)
         {
