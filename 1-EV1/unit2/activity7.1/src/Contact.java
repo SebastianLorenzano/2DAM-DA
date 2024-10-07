2e77b72ee4226e194e3b688559124cdbf8021a92
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2024-10-07 19:28:16 +0200

    started activity7.1
    will have to change things because I made a ContactList, and to use what I made I need to be able to read a document and parse it to an object, so I will instead not use a list for this exercise and save the code for later.

---
+++ 1-EV1/unit2/activity7.1/src/Contact.java
@@ -0,0 +1,90 @@
+public class Contact
+{
+    private String name;
+    private String surname;
+    private String email;
+    private String number;
+    private String description;
+
+    public Contact(String name, String surname, String email,
+                    String number, String description)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
+
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public boolean isValid()
+    {
+        return isValid(name, surname, email, number, description);
+    }
+    public static boolean isValid(Contact contact)
+    {
+        return isValid(contact.name, contact.surname, contact.email, contact.number, contact.description);
+    }
+
+    public static boolean isValid(String name, String surname, String email,
+                                  String number, String description)
+    {
+        // check if params are valid
+        return true;
+    }
+
+    public Contact clone()
+    {
+        return new Contact(name, surname, email, number, description);
+    }
+
+}
---
+++ 1-EV1/unit2/activity7.1/src/ContactList.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ContactList
+{
+    private ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+    public int count()
+    {
+        return contacts.size();
+    }
+
+    public Contact getAt(int index)
+    {
+        return index >= 0 && index < contacts.size() ? contacts.get(index).clone() : null;
+    }
+
+    public boolean add(Contact contact)
+    {
+        if (contact != null && contact.isValid())
+        {
+            contacts.add(contact.clone());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean contains(Contact contact)
+    {
+        return indexOf(contact) >= 0;
+    }
+
+    public int indexOf(Contact contact)
+    {
+        if (contact == null)
+            return -1;
+        for (int x = 0; x < contacts.size(); x++)
+        {
+            Contact c = contacts.get(x);
+            if (contact.getEmail().equals(c.getEmail()))
+                return x;
+        }
+        return -1;
+    }
+
+    public ArrayList<Contact> filter(Predicate<Contact> predicate) {
+        ArrayList<Contact> result = new ArrayList<>();
+        for (Contact contact : contacts)
+            if (predicate.test(contact))
+                result.add(contact.clone());
+        return result;
+    }
+
+    public ArrayList<Contact> filterByFullName(String name, String surname)
+    {
+        return filter(contact -> contact.getName().equals(name) && contact.getSurname().equals(surname));
+    }
+
+    public ArrayList<Contact> filterByNumber(String number)
+    {
+        return filter(contact -> contact.getNumber().equals(number));
+    }
+
+    private void remove(int index)
+    {
+        if (index >= 0 && index < contacts.size())
+            contacts.remove(index);
+    }
+    public void remove(Contact contact)
+    {
+        remove(indexOf(contact));
+    }
+
+
+
+
+
+
+
+}
