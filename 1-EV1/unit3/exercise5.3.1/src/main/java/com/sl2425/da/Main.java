bd310d79d61dae82b28170970978979d5a0e64e9
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2024-10-15 20:44:39 +0200

    finished a lot of exercises (description)
    finished exercise4.1.1, 4.1.2, 4.1.3, 5.3.1, 6.1.1

---
+++ 1-EV1/unit3/exercise5.3.1/src/main/java/com/sl2425/da/Main.java
@@ -0,0 +1,27 @@
+package com.sl2425.da;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConsultaBD1(null);
+    }
+    public static void ConsultaBD1(String[] args)
+            throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/VTInstitute_SL_2425";
+        String user = "postgres";
+        String password = "postgres";
+        String SQLsentence = "INSERT INTO subjects (Name, Year, Hours) VALUES (?, ?, ?)";
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = con.prepareStatement(SQLsentence))
+        {
+            preparedStatement.setString(1, "TEST_ASIGNATURE");
+            preparedStatement.setInt(2, 0);
+            preparedStatement.setInt(3, 0);
+            int result = preparedStatement.executeUpdate();
+            System.out.println(result);
+
+        }
+
+    }
+}
