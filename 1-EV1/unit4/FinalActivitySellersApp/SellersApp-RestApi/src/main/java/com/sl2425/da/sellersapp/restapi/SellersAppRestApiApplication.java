ab6b4374e86acfa2770cc1b4bbd523d327ad2344
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2024-12-17 20:11:45 +0100

    17-12-24

---
+++ 1-EV1/unit4/FinalActivitySellersApp/SellersApp-RestApi/src/main/java/com/sl2425/da/sellersapp/restapi/SellersAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sl2425.da.sellersapp.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellersAppRestApiApplication.class, args);
+	}
+
+}
---
+++ 1-EV1/unit4/FinalActivitySellersApp/SellersApp-RestApi/src/test/java/com/sl2425/da/sellersapp/restapi/SellersAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl2425.da.sellersapp.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellersAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
