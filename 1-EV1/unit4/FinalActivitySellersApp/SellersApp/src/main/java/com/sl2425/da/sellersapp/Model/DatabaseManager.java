eac7d58891933895060ca86cbdd15d28044b5ad1
SebastianLorenzano <selorenzano3@gmail.com>
2025-01-10 14:34:12 +0100

    10-01-25

---
+++ 1-EV1/unit4/FinalActivitySellersApp/SellersApp/src/main/java/com/sl2425/da/sellersapp/Model/DatabaseManager.java
@@ -0,0 +1,24 @@
+package com.sl2425.da.sellersapp.Model;
+
+import com.sl2425.da.sellersapp.Model.Entities.CategoryEntity;
+import com.sl2425.da.sellersapp.Model.Entities.ProductEntity;
+import com.sl2425.da.sellersapp.Model.Entities.SellerEntity;
+import com.sl2425.da.sellersapp.Model.Entities.SellerProductEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public abstract class DatabaseManager
+{
+    public abstract SellerEntity SelectSellerWithCifAndPassword(String cif, String password);
+    public abstract boolean updateSeller(SellerEntity updatedSeller);
+    public abstract List<CategoryEntity> SelectCategories();
+    public abstract List<SellerProductEntity> SelectSellerProducts(SellerEntity seller);
+    public abstract List<ProductEntity> SelectAvailableProducts(SellerEntity seller, CategoryEntity category);
+    public abstract boolean InsertSellerProduct(SellerEntity seller, ProductEntity product, BigDecimal price, int stock);
+    public abstract boolean AddOffer(SellerProductEntity updatedSellerProduct);
+
+
+
+
+}
---
+++ 1-EV1/unit4/FinalActivitySellersApp/SellersApp/src/main/java/com/sl2425/da/sellersapp/Model/GenericAppController.java
@@ -0,0 +1,14 @@
+package com.sl2425.da.sellersapp.Model;
+
+import com.sl2425.da.sellersapp.Model.Entities.SellerEntity;
+
+import java.util.logging.Logger;
+
+public class GenericAppController
+{
+    protected DatabaseManager database;
+    protected SellerEntity currentSeller;
+    protected Logger logger;
+
+
+}
