964bd536bf6d729678097ee9f2f46f8a54599616
SebastianLorenzano <selorenzano3@gmail.com>
2025-02-14 19:43:15 +0100

    14-02-25

--- 1-EV1/unit4/FinalActivitySellersApp/SellersApp-RestApi/src/main/java/com/sl2425/da/sellersapp/restapi/services/SellerProductServices.java
+++ 1-EV1/unit4/FinalActivitySellersApp/SellersApp-RestApi/src/main/java/com/sl2425/da/sellersapp/restapi/services/SellerProductServices.java
@@ -30,11 +30,7 @@ public class SellerProductServices
     @Autowired
     private ISellerProductEntityDAO sellerProductDAO;
     @Autowired
-    private ISellerEntityDAO sellerDAO;
-    @Autowired
     private IProductEntityDAO productDAO;
-    @Autowired
-    private ICategoryEntityDAO categoryDAO;
     @Autowired
     private SellerServices sellerServices;
     @Autowired
