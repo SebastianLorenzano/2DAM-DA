e19514a68f91b1eacd930f3e7f7bcede8095a814
SebastianLorenzano <selorenzano3@gmail.com>
2025-02-19 18:57:02 +0100

    second exercise (not checked)

--- 1-EV1/unit4/FinalActivitySellersApp/SellersApp-RestApi/src/main/java/com/sl2425/da/sellersapp/restapi/controllers/web/SellerProductListBySellerWebController.java
+++ 1-EV1/unit4/FinalActivitySellersApp/SellersApp-RestApi/src/main/java/com/sl2425/da/sellersapp/restapi/controllers/web/SellerProductListBySellerWebController.java
@@ -1,4 +1,55 @@
 package com.sl2425.da.sellersapp.restapi.controllers.web;
 
-public class SellerProductListBySellerWebController {
+
+import com.sl2425.da.sellersapp.Model.Entities.CategoryEntity;
+import com.sl2425.da.sellersapp.Model.Entities.SellerEntity;
+import com.sl2425.da.sellersapp.Model.Entities.SellerProductEntity;
+import com.sl2425.da.sellersapp.restapi.model.codeStatus.LoginCodeStatus;
+import com.sl2425.da.sellersapp.restapi.services.CategoryServices;
+import com.sl2425.da.sellersapp.restapi.services.ProductServices;
+import com.sl2425.da.sellersapp.restapi.services.SellerProductServices;
+import com.sl2425.da.sellersapp.restapi.services.SellerServices;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/web")
+public class SellerProductListBySellerWebController
+{
+
+    @Autowired
+    private ProductServices productServices;
+    @Autowired
+    CategoryServices categoryServices;
+    @Autowired
+    private SellerServices sellerServices;
+    @Autowired
+    private SellerProductServices sellerProductServices;
+
+
+    @GetMapping({"/sellerProducts/showProducts"})
+    public String showSellerProductsBySeller(@AuthenticationPrincipal UserDetails user, Model model)
+    {
+        Pair<Optional<SellerEntity>, LoginCodeStatus> pair = sellerServices.getSellerByCif(user.getUsername());
+
+        if (pair.getLeft().isEmpty())
+        {
+            model.addAttribute("error", "Seller not found");
+            return "index";
+        }
+
+        List<SellerProductEntity> sellerProducts = sellerProductServices.findAllSellerProductsBySeller(pair.getLeft().get());
+        model.addAttribute("sellerProducts", sellerProducts);
+
+        return "sellerProducts-showProducts";
+    }
 }
