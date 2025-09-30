3d4a837aa5f112d5c5143dcd71023f572f3e8326
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2025-09-30 23:10:54 +0200

    cleared some old code

--- 1-EV2/0-demo1Profe/src/main/java/com/jrgs2122/unit6/demo1/model/dao/IDeptEntityDAO.java
+++
@@ -1,7 +0,0 @@
-package com.jrgs2122.unit6.demo1.model.dao;
-
-import com.jrgs2122.unit6.demo1.model.entities.DeptJPAEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IDeptEntityDAO extends CrudRepository<DeptJPAEntity, Integer> {
-}
