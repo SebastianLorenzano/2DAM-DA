1d8c89620da3a009ff7a0908a556437ecd7ac1c8
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2025-10-01 23:43:37 +0200

    deleting old code

--- 1-EV1/0-trash/demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.sl2425.da.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.jfr;
-
-
-    opens com.sl2425.da.demo to javafx.fxml;
-    exports com.sl2425.da.demo;
-}
